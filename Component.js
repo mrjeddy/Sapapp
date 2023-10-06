0260578f39c3264334f07e9effe1549b0ef7383d
mrjeddy <mrjeddy.ab@gmail.com>
2023-10-06 21:42:57 +0500

    Initial commit

---
+++ Component.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+], function (UIComponent) {
+	"use strict";
+
+	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+
+		metadata : {
+            invoice: "json",
+			manifest: "json"		
+		},
+
+		init : function () {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+
+		}
+	});
+
+});
